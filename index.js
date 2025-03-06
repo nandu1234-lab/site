cbe1f75618f96793ca07b7fe7983ece75279da04
nandu1234-lab <nandukrishnaam@gmail.com>
2025-03-06 15:27:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("\nHello\nWorld!")
+console.log("Hello !")
+console.log("Hel")
+console.log("hyy!")
+console.log("Word!")
+console.log("ha")
+console.log("World")
+console.log("hi")
+console.log("ho!")
+console.log("World")
+let bodyCount=10
+const pie=3.14
+console.log(bodyCount)
+console.log(pie)
+console.log(10+5)
+console.log(10-5)
+console.log(10*5)
+console.log(10/5)
+console.log(10%5)
+let a=69
+let b=420
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
